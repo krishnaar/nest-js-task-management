d4fc01d4d0a284e02a2234ad62b28e377aef2598
krishnaar <krishnaalrasyid@gmail.com>
2021-01-06 14:40:42 +0700

    search and filtering

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -8,9 +9,15 @@ export class TasksController {
     constructor(private TasksService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.TasksService.getAllTasks();
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if (Object.keys(filterDto).length) {
+            return this.TasksService.getTaskWithFilters(filterDto);
+        }else{
+
+            return this.TasksService.getAllTasks();
+        }
     }
+
 
     @Get('/:id')
     getTaskById(@Param('id') id: string) {
