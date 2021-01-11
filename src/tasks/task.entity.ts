545c483cde84f9facdaf62c4b534343cc04d5a39
krishnaar <krishnaalrasyid@gmail.com>
2021-01-11 16:25:36 +0700

    preparation for task service refactoring

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,5 @@
 import { BaseEntity, Entity, Column,  PrimaryGeneratedColumn } from "typeorm";
-import { TaskStatus } from "./task.model";
+import { TaskStatus } from "./task-status.enum";
 
 @Entity()
 export class Task extends BaseEntity {
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,45 +2,44 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, Val
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
-import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private TasksService: TasksService) {}
 
-    @Get()
-    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-        if (Object.keys(filterDto).length) {
-            return this.TasksService.getTaskWithFilters(filterDto);
-        }else{
+    // @Get()
+    // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+    //     if (Object.keys(filterDto).length) {
+    //         return this.TasksService.getTaskWithFilters(filterDto);
+    //     }else{
 
-            return this.TasksService.getAllTasks();
-        }
-    }
+    //         return this.TasksService.getAllTasks();
+    //     }
+    // }
 
 
-    @Get('/:id')
-    getTaskById(@Param('id') id: string) {
-        return this.TasksService.getTaskById(id);
-    }
+    // @Get('/:id')
+    // getTaskById(@Param('id') id: string) {
+    //     return this.TasksService.getTaskById(id);
+    // }
 
-    @Post()
-    @UsePipes(ValidationPipe)
-    createTask(@Body() createTaskDto: CreateTaskDto): Task {
-        return this.TasksService.createTask(createTaskDto);
-    }
+    // @Post()
+    // @UsePipes(ValidationPipe)
+    // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    //     return this.TasksService.createTask(createTaskDto);
+    // }
 
-    @Delete('/:id')
-    deleteTask(@Param('id') id: string): void {
-        this.TasksService.deleteTask(id);
-    }
+    // @Delete('/:id')
+    // deleteTask(@Param('id') id: string): void {
+    //     this.TasksService.deleteTask(id);
+    // }
 
-    @Patch('/:id/status')
-    updateTaskStatus (
-        @Param('id') id: string,
-        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-    ): Task {
-        return this.TasksService.updateTaskStatus(id, status);
-    }
+    // @Patch('/:id/status')
+    // updateTaskStatus (
+    //     @Param('id') id: string,
+    //     @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    // ): Task {
+    //     return this.TasksService.updateTaskStatus(id, status);
+    // }
 }
