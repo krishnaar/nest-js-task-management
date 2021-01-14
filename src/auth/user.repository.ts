512e7a0a8086c4dd8f3b6241c94c7a15c3265061
krishnaar <krishnaalrasyid@gmail.com>
2021-01-14 15:03:33 +0700

    hash with salt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,6 @@
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
 
@@ -10,7 +11,11 @@ export class UserRepository extends Repository<User> {
 
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
+
+        console.log(user.password);
+        
 
         try{
             await user.save();
@@ -21,5 +26,9 @@ export class UserRepository extends Repository<User> {
                 throw new InternalServerErrorException();
             }
         }
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string>{
+        return bcrypt.hash(password, salt);
     }
 }
